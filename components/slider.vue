1b2e31b7a4f60d592f38ced71dd87ecfaa12d7f2
zekainhuman1 <92748539+zekainhuman1@users.noreply.github.com>
2023-04-27 14:32:44 +0300

    Update slider.vue
    asdasdasd

--- components/slider.vue
+++ components/slider.vue
@@ -99,6 +99,22 @@ export default {
 </script>
 
   <style>
+  .custom-arrow_prev {
+  background-color: #fff;
+  border: 1px solid #fff;
+  border-radius: 50%;
+  box-shadow: -13px 20px 10px rgba(0, 0, 0, 0.01),
+    -7px 11px 8px rgba(0, 0, 0, 0.05), -3px 5px 6px rgba(0, 0, 0, 0.09),
+    -1px 1px 3px rgba(0, 0, 0, 0.1), 0 0 0 rgba(0, 0, 0, 0.1);
+  content: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAOCAYAAAASVl2WAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAABmSURBVHgBrZHLCQAhDESdBcGLYglpxcotRfsQzMabiyFediCQ8IaQD5yinDPNOSszu8eAtGoYsI8xygemlFqMkSVaCIF+gEsmFB1bHAZRAdAlJ+991brc57BM10NB67SboE2+P+sFFoZMIq8PPtIAAAAASUVORK5CYII=);
+  font-size: 30px;
+  height: 10px;
+  -webkit-mask-repeat: no-repeat;
+  mask-repeat: no-repeat;
+  padding: 13px;
+  width: 10px;
+}
+  
 .ban_slider {
   width: 840px;
 }
@@ -142,10 +158,10 @@ export default {
   position: relative;
   transform: translateZ(0);
 }
-/* element.style {
+ .element.style {
     width: 4200px;
     opacity: 1;
-    } */
+    }
 .slick-track {
   display: block;
   left: 0;

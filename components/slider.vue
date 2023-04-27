427e7ad8ac17b0e1c331de7efb527b29535c830c
zekainhuman1 <92748539+zekainhuman1@users.noreply.github.com>
2023-04-27 15:05:25 +0300

    Update slider.vue
    sdfdsf

--- components/slider.vue
+++ components/slider.vue
@@ -72,7 +72,7 @@
 import VueSlickCarousel from "vue-slick-carousel";
 import "vue-slick-carousel/dist/vue-slick-carousel.css";
 // optional style for arrows & dots
-import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
+// import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
 
 export default {
   name: "MyComponent",
